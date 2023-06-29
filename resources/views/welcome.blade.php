85eb7a0d44f33fadc70c9b27a60b4bd4e8d22521
Marco Giammari <marcogiammari@gmail.com>
2023-06-29 16:27:42 +0200

    add table "partenze"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,47 @@
     @vite('resources/js/app.js')
 </head>
 
-<body class="antialiased">
-    <div class="flex justify-center items-center h-screen">
-        <h1 class="text-3xl text-purple-600 font-bold">TRAINS</h1>
-        <pre>
-            @dd($trains)
-        </pre>
+<body class="antialiased bg-slate-700">
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h1 class="text-3xl text-gray-300 font-bold p-2">PARTENZE</h1>
+        <table class="bg-slate-900">
+            <thead>
+                <tr class="text-yellow-400 text-left">
+                    <th scope="col" colspan="2">Treno</th>
+                    <th scope="col">Destinazione</th>
+                    <th scope="col">Orario</th>
+                    <th scope="col">In orario</th>
+                    <th scope="col">Cancellato</th>
+                    <th scope="col">Carrozze</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr class="text-yellow-500">
+                        <td>{{ ucfirst($train->azienda) }}</td>
+                        <td>{{ $train->codice_treno }}</td>
+                        <td>{{ ucfirst($train->stazione_di_arrivo) }}</td>
+                        <td>{{ $train->orario_di_partenza }}</td>
+                        <td>
+                            @if ($train->in_orario == 1)
+                                Sì
+                            @else
+                                No
+                            @endif
+                        </td>
+                        <td>
+                            @if ($train->cancellato == 1)
+                                Sì
+                            @else
+                                No
+                            @endif
+                        </td>
+                        <td>{{ $train->numero_carrozze }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     </div>
 </body>
 
