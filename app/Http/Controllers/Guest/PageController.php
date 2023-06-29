e7e93474fcff9516c3fa769df64776d27538889a
Marco Giammari <marcogiammari@gmail.com>
2023-06-29 18:23:30 +0200

    add (PageController) select, where, orderBy

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,6 +11,7 @@ class PageController extends Controller
     public function index()
     {
         $trains = Train::all();
+        $trains = Train::select('azienda', 'codice_treno', 'stazione_di_arrivo', 'orario_di_partenza', 'in_orario', 'numero_carrozze', 'tipo_treno', 'binario')->where('stazione_di_partenza', 'Roma')->orderBy('orario_di_partenza', 'ASC')->get();
 
         return view('welcome', compact('trains'));
     }
